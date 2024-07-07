bc1d57c5b540c680b87481b6c114031b48e75f81
BuildTools <unconfigured@null.spigotmc.org>
2024-07-07 14:03:41 +0530

    response.text()

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ SQLDetails.addEventListener("submit", async function (event)
     event.preventDefault()
 
     let response = await fetch("https://expensetracker-amaankazi.onrender.com/status")
-    console.log(response.body)
+    console.log(response.text())
 
     data = SQLDetails.Data.value
 })
@@ -33,5 +33,5 @@ async function Retrieve()
         body: JSON.stringify({ password: "test123", task: "exampleTask" })
     })
 
-    console.log(response.body)
+    console.log(response.text())
 }

4a5401fc6a1beb241949bb443f4b369ea80a5f43
BuildTools <unconfigured@null.spigotmc.org>
2024-07-28 20:48:44 +0530

    query string

---
+++ js/expenses.js
@@ -0,0 +1,25 @@
+const htmlBody = document.getElementById("htmlBody");
+const DarkThemeIcon = document.getElementById("DarkThemeIcon");
+const LightThemeIcon = document.getElementById("LightThemeIcon");
+
+var queryString = Window.location.search();
+console.log(queryString); 
+
+function ToggleTheme()
+{
+    let theme = htmlBody.getAttribute("data-bs-theme");
+
+    if (theme == "light")
+    {
+        LightThemeIcon.style.display = "none";
+        DarkThemeIcon.style.display = "inline-block";
+        htmlBody.setAttribute("data-bs-theme", "dark");
+    }
+    else
+    {
+        LightThemeIcon.style.display = "inline-block";
+        DarkThemeIcon.style.display = "none";
+        htmlBody.setAttribute("data-bs-theme", "light");
+    }
+}
+

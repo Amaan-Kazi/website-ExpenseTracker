a5c2b7a251d66b655ee5b562eef16d3a22fb7c63
Amaan-Kazi <amaankazi1793@gmail.com>
2024-08-12 22:28:24 +0530

    authenticate on load

--- js/expenses.js
+++ js/expenses.js
@@ -2,8 +2,43 @@ const htmlBody = document.getElementById("htmlBody");
 const DarkThemeIcon = document.getElementById("DarkThemeIcon");
 const LightThemeIcon = document.getElementById("LightThemeIcon");
 
-var queryString = window.location.search;
-console.log(queryString); 
+var userInfo = JSON.parse(localStorage.getItem("userInfo"));
+
+if (userInfo != null) {
+    let response = await fetch(`https://amaankazi-expensetracker.onrender.com/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: userInfo.email,
+            password: userInfo.inputPassword
+        })
+    });
+    let responseData = await response.json();
+
+    if (responseData.status == "SUCCESSFUL")
+    {
+        userInfo.userName = responseData.userName;
+        localStorage.setItem("userInfo", JSON.stringify(userInfo));
+
+        toast("Login", "Successful", `You have succesfully logged in as<br><span style = "color: #3987fd;">${responseData.userName} [${login.emailId.value}]</span>`);
+    }
+    else if (responseData.status == "SERVER ERROR")
+    {
+        toast("Login", "ERROR", "SERVER ERROR");
+        window.location.href = "./login.html";
+    }
+    else
+    {
+        toast("Login", "ERROR", responseData.error);
+        window.location.href = "./login.html";
+    }
+}
+else
+{
+    window.location.href = "./login.html";
+}
 
 function ToggleTheme()
 {

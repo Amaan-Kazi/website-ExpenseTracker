33c1353578a02d01dbb722f5227dde406f48e3df
Amaan Kazi <amaankazi1793@gmail.com>
2024-08-24 00:56:47 +0530

    Log out

--- js/expenses.js
+++ js/expenses.js
@@ -81,20 +81,41 @@ async function getTables()
         membersList += "</ul>";
 
         mainContent.innerHTML += `
-            <div class="card" style="width: 30rem;">
+            <div class="card sheets" style="width: 30rem;">
                 <div class="card-body">
                     <h5 class="card-title text-info fs-1">${responseData.response[i].sheetname}</h5>
                     <p class="card-text fs-3" style="margin: 0;">
                         Members:
                         ${membersList}
                     </p>
-                    <button class = "btn btn-primary" style="width: 100%;">Dashboard</button>
+                    <div class="btn-group" style="width: 100%;">
+                        <button type="button" class="btn btn-primary" style="width: 90%;">Dashboard</button>
+                        <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="visually-hidden">Toggle Dropdown</span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Actions</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><button class="dropdown-item text-danger" onclick="DeleteSheet()">Delete</button></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         `
     }
 
     // Add message to display when user has no tables
+}
+
+function LogOut()
+{
+    localStorage.removeItem("userInfo");
+    window.location.href("./login.html");
+}
+
+function DeleteSheet()
+{
+    console.log("Deleting Sheet");
 }
 
 Authenticate();

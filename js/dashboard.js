6137c3bdd1e5d47168823545d80a0b055b628f04
Amaan Kazi <amaankazi1793@gmail.com>
2024-09-16 23:50:21 +0530

    chart js and dasboard

---
+++ js/dashboard.js
@@ -0,0 +1,4 @@
+const htmlBody = document.getElementById("htmlBody");
+const DarkThemeIcon = document.getElementById("DarkThemeIcon");
+const LightThemeIcon = document.getElementById("LightThemeIcon");
+

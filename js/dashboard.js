3dacb75f643072257474f203a576cccc0750697a
Amaan Kazi <amaankazi1793@gmail.com>
2024-09-18 16:42:14 +0530

    Header

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,3 +2,24 @@ const htmlBody = document.getElementById("htmlBody");
 const DarkThemeIcon = document.getElementById("DarkThemeIcon");
 const LightThemeIcon = document.getElementById("LightThemeIcon");
 
+var userInfo = JSON.parse(localStorage.getItem("userInfo"));
+
+function ToggleTheme()
+{
+    let theme = htmlBody.getAttribute("data-bs-theme");
+
+    if (theme == "light")
+    {
+        LightThemeIcon.style.display = "none";
+        DarkThemeIcon.style.display = "inline-block";
+        htmlBody.setAttribute("data-bs-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    }
+    else
+    {
+        LightThemeIcon.style.display = "inline-block";
+        DarkThemeIcon.style.display = "none";
+        htmlBody.setAttribute("data-bs-theme", "light");
+        localStorage.setItem("theme", "light");
+    }
+}
